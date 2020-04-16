ee6e6b86b94709bc6a49a490e10ee33cdfa04735
Antoine Chassagne <antoine.chassagne@me.com>
2020-04-16 17:56:53 +0200

    Updates

---
+++ precache-manifest.9d84a744ad07350927b379f78b6c2f57.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "119450fa17f7b01504886ef98781f870",
+    "url": "/saint-jacques/index.html"
+  },
+  {
+    "revision": "dd7e4475c84ffdb5f2bc",
+    "url": "/saint-jacques/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "d41a7060edf60eca40f6",
+    "url": "/saint-jacques/static/js/2.94109b33.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/saint-jacques/static/js/2.94109b33.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd7e4475c84ffdb5f2bc",
+    "url": "/saint-jacques/static/js/main.6413bd8d.chunk.js"
+  },
+  {
+    "revision": "57599aa95d03ce279f79",
+    "url": "/saint-jacques/static/js/runtime-main.92ccb2f1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/saint-jacques/static/media/logo.5d5d9eef.svg"
+  }
+]);
