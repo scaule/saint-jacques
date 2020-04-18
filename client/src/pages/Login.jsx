89c7404fd35becab120a34b5f821fecb27e9d2fd
Antoine Chassagne <antoine.chassagne@me.com>
2020-04-18 15:44:04 +0200

    Add Login page component

---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from "react";
+import Api from "../services/Api";
+
+const Login = () => {
+  document.title = "Saint Jacques | Connexion";
+
+  return (
+    <>
+      <h1>Se connecter</h1>
+    </>
+  );
+};
+
+export default Login;
